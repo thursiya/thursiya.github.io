d5293b9261ad5d5d6fb1fee2ff1027cbbb81335a
thursiya <thursiya.exe@gmail.com>
2025-01-15 21:19:10 -0800

    Add files via upload

---
+++ skunkland/Aiur.php
@@ -0,0 +1,15 @@
+<?php $nation="Aiur"; $ntype="Kingdom"; include '../data/header-world.htm'; ?>
+
+	<script>
+		infowindow("","The Unnamed City","","Aiuric","Absentee Monarchy","","","","","Unnamed City Cathedral (34m)","Unnamed City Fortress (54m)");
+	</script>
+		
+	<p>
+		Aiur was founded in <script>sy(2014,5,15,13)</script>.  It is located in the deep south 
+		of Skunkland on the southern shores of the Austral ocean.
+	</p>
+	<br>
+	
+	<div style="clear:both"></div>
+
+<?php include '../data/footer.htm'; ?>
