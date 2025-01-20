953ad32b2eb451309098927da8fb56ae3c2275f6
thursiya <thursiya.exe@gmail.com>
2025-01-19 18:04:11 -0800

    Update time.js

--- skunkland/data/time.js
+++ skunkland/data/time.js
@@ -1,27 +1,11 @@
-Date.prototype.getDOY = function() {
-	const onejan = new Date(this.getFullYear(), 0, 1);
-	return (this - onejan) / 86400000;
-}
-
 function syear(day,val2,val3,val4) {
 	if (typeof day == 'number') {
 		day = new Date(day, (val2 - 1), (val3 || 1), (val4 || 0));
 		if (!val3) val2 = 'noday';
 	}
-	const longsy = (day.getFullYear() - 2012 + (day.getDOY() + 61) / 365) * 72 + 1;
-	const syr = ~~(longsy);
+	const longsy = (day.getFullYear() - 2012 + ((day - new Date(day.getFullYear(),0,1)) / 86400000 + 61) / 365) * 72 + 1;
 	const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	const skunkmonth = ~~(longsy % 1 * 12);
-	let out = `<span title='${month[day.getMonth()]} `;
-	if (val2 != "noday") {
-		out += `${day.getDate()}, `;
-	}
-	out += `${day.getFullYear()}'>`;
-	if (val2 == "full" || val4) {
-		out += `${month[skunkmonth]} of `;
-	}
-	out += `SY ${syr}</span>`;
-	return out;
+	return `<span title='${month[day.getMonth()]} ${val2 != "noday" ? `${day.getDate()}, ` : ""}${day.getFullYear()}'>${val2 == "full" || val4 ? `${month[~~(longsy % 1 * 12)]} of ` : ""}SY ${~~longsy}</span>`;
 }
 
 function sy(a,b,c,d) { document.write(syear(a,b,c,d)) }
