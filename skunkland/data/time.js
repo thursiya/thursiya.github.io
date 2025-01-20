e886a848eb961b746dc2ce5ff2bf2842b0d87339
thursiya <thursiya.exe@gmail.com>
2025-01-19 18:24:20 -0800

    Update time.js

--- skunkland/data/time.js
+++ skunkland/data/time.js
@@ -3,9 +3,8 @@ function syear(day,val2,val3,val4) {
 		day = new Date(day, (val2 - 1), (val3 || 1), (val4 || 0));
 		if (!val3) val2 = 'noday';
 	}
-	const longsy = (day.getFullYear() - 2012 + ((day - new Date(day.getFullYear(),0,1)) / 86400000 + 61) / 365) * 72 + 1;
-	const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	return `<span title='${month[day.getMonth()]} ${val2 != "noday" ? `${day.getDate()}, ` : ""}${day.getFullYear()}'>${val2 == "full" || val4 ? `${month[~~(longsy % 1 * 12)]} of ` : ""}SY ${~~longsy}</span>`;
+	const lsy = (day.getFullYear() - 2012 + ((day - new Date(day.getFullYear(),0,1)) / 86400000 + 61) / 365) * 72 + 1;
+	return `<span title='${day.toLocaleString("en",{month:"long"})} ${val2 != "noday" ? `${day.getDate()}, ` : ""}${day.getFullYear()}'>${val2 == "full" || val4 ? `${new Date(~~lsy, ~~(lsy % 1 * 12)).toLocaleString("en",{month:"long"})} of ` : ""}SY ${~~lsy}</span>`;
 }
 
 function sy(a,b,c,d) { document.write(syear(a,b,c,d)) }
