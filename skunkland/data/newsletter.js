19878b2d5d9a3d9c981a94d4fefb2a3b254a7a8d
thursiya <thursiya.exe@gmail.com>
2025-01-28 15:17:40 -0800

    Update newsletter.js

--- skunkland/data/newsletter.js
+++ skunkland/data/newsletter.js
@@ -2,3 +2,7 @@ function pic(name, loc = "gallery") {
 	const file = {gallery: "../gallery/images", newsletter: "images", map: "../maps/political", specialmap: "../maps/special"};
 	document.write(`<a href='${file[loc]}/${name}.jpg' target='_blank'><img src='${file[loc]}/${name}.jpg' alt='${name}' height='300'></a>`);
 }
+
+function wiki(str) {
+	document.write(`<a href="https://minecraft.wiki/w/${str}">${str}</a>`);
+}
