d10a2352f2111b938aa9608b9eaa7609cc020fe9
thursiya <thursiya.exe@gmail.com>
2025-01-30 10:19:23 -0800

    Update newsletter.js

--- skunkland/data/newsletter.js
+++ skunkland/data/newsletter.js
@@ -1,3 +1,7 @@
+function note(str) {
+	document.write(`<span style="color: #999; font-size: 75%; font-style: italic">(${str})</span>`);
+}
+
 function pic(name, loc = "gallery") {
 	const file = {gallery: "../gallery/images", newsletter: "images", map: "../maps/political", special: "../maps/special"};
 	document.write(`<a href='${file[loc]}/${name}.jpg' target='_blank'><img src='${file[loc]}/${name}.jpg' alt='${name}' height='300' style='margin-left: 20px'></a>`);
