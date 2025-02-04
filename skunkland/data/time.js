f45c2f8a7de8428172b8bf59aa53d42c5fd69e54
thursiya <thursiya.exe@gmail.com>
2025-02-03 20:07:54 -0800

    Update time.js

--- skunkland/data/time.js
+++ skunkland/data/time.js
@@ -1,6 +1,6 @@
 function syear(day, val2, val3, val4) {
 	if (typeof day == 'number') {
-		day = new Date(day, (val2 - 1), (val3 || 1), (val4 || 0));
+		day = new Date(day, (val2 - 1), (val3 || 1), (val4 || 1));
 		if (!val3) val2 = 'noday';
 	}
 	const lsy = (day.getFullYear() - 2012 + ((day - new Date(day.getFullYear(),0,1)) / 86400000 + 61) / 365) * 72 + 1;
