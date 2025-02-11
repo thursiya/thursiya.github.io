d4b61f95e65e7686393068108f83fd8ef060b069
thursiya <thursiya.exe@gmail.com>
2025-02-10 18:05:07 -0800

    Create world.js

---
+++ skunkland/data/world.js
@@ -0,0 +1,43 @@
+function infowindow(nation, ntype, motto, capital, bigcity, demonym, government, language, money, faith, animal, tallbuilding, tallstructure) {
+  bigcity = bigcity || capital;
+  
+	function subTable(...arr) {
+    let stOut = `<tr><td style="padding: 5px;"><table class="info">`;
+    arr.forEach(v => stOut += `<tr><td style="${v[0] == "Government" ? " font-size: 11px; " : ""}font-weight: bold; width: 90px;">${v[0]}</td><td>${v[1]}</td></tr>`);
+    return `${stOut}</table></td></tr>`;
+  }
+  
+  let out = `<table style="width: 300px;"><tr><th>${ntype} of ${nation}</th></tr>
+    <tr><td><img src="../images/${nation} Flag (Wool).png" width="150"><br><i>${motto || "---"}</i></td></tr>
+    <tr><td style="padding: 0;"><img src="../../maps/regional/${nation}.jpg" width="298"></td></tr>
+    ${subTable(["Capital", capital], ["Largest City", `${bigcity} (${pp(bigcity)})`])}
+	  ${subTable(["Demonym", demonym])}
+    ${subTable(["Government", government])}`;
+  const arr = [];
+  if (language) arr.push(["Language", language]);
+  if (money) arr.push(["Currency", money]);
+  if (faith) arr.push(["Religion", faith]);
+  if (arr[0]) out += subTable(arr);
+  if (animal) out += subTable(["Animal", animal]);
+  out += `${subTable(["Tallest Building", tallbuilding], ["Tallest Structure", tallstructure])}
+    <tr><td><img src="../images/${nation} Banner.png" width="64"></td></tr></table>`;
+  
+  document.getElementById("infowindow").innerHTML = out;
+}
+
+function regions(nation, rtype, regions, bigsettles, builds, nflag) {
+  var out = "<table><tr><th>Flag</th><th>" + rtype + "</th><th>Largest<br>Settlement</th><th>Notable Builds</th></tr>";
+		for (var i = 0; i < regions.length; i++) {
+			var rname = regions[i];
+			if (nflag) rname = nation + " " + regions[i];
+			out += "<tr><td><img src='images/" + rname + " Flag.png' height='50' alt='Flag of " + rname + " " + rtype + "'></td>";
+			out += "<td><a href='" + nation + "/" + regions[i] + ".php'><b>" + regions[i] + "</b></a></td>";
+			out += "<td>" + (bigsettles[i] || "-") + "</td><td>";
+			for (var j = 0; j < builds[i].length; j++) {
+				out += builds[i][j] + "<br>"
+			}
+			out += "</td></tr>";
+		}
+		out += "</table><br>";
+		document.write(out);
+}
