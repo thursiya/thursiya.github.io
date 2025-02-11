349f9c63a87e112021dd605c08a165286d1c32d3
thursiya <thursiya.exe@gmail.com>
2025-02-10 22:55:00 -0800

    Update world.js

--- skunkland/data/world.js
+++ skunkland/data/world.js
@@ -8,9 +8,8 @@ loadDoc(`${homepage}data/nations.txt`, buildNationArray);
 function buildCityArray(xhr) {
 	city.push(...xhr.response.split('\n').filter(v => v).
 		map(v => v.replace(/\s*,\s*/g, ',').trim().split(',')).
-		map(v => ({name: v[0], state: v[1], pop: Math.ceil(((Number(v[2]) / 2 + Number(v[3])) * (Number(v[5]) || 1) + Number(v[4]) / 2) * 10) * 100, lastCensus: new Date(v[6], Number(v[7]) - 1, v[8])})));//.
-		//map(v => Object.assign(v, {})[v[1], v[0], Math.ceil(((Number(v[2]) / 2 + Number(v[3])) * (Number(v[5]) || 1) + Number(v[4]) / 2) * 10) * 100, new Date(v[6], Number(v[7]) - 1, v[8])]).
-		//map(v => [...v, v[0] ? `<a href="${v[0]}">${v[0]}</a>` : "<i>Skunkland</i>", v[2].toLocaleString("en", {useGrouping: true}), syear(v[3])]));
+		map(v => ({name: v[0], state: v[1], pop: Math.ceil(((Number(v[2]) / 2 + Number(v[3])) * (Number(v[5]) || 1) + Number(v[4]) / 2) * 10) * 100, date: new Date(v[6], Number(v[7]) - 1, v[8])})).
+		map(v => Object.assign(v, {output: {state: v.state ? `<a href="${v.state}">${v.state}</a>` : "<i>Skunkland</i>", pop: v.pop.toLocaleString("en", {useGrouping: true}), date: syear(v.date)])));
 	console.log(city);
 }
 
