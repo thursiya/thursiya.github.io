080b81bae3f897ecd313a935b8fc83a665ef3e76
thursiya <thursiya.exe@gmail.com>
2025-02-12 20:18:13 -0800

    Update Crevatia.php

--- skunkland/world/Crevatia.php
+++ skunkland/world/Crevatia.php
@@ -1,44 +1,56 @@
-<?php $nation="Crevatia"; $ntype="Democratic People's Republic"; include '../data/header-world.htm' ?>
-
-	<p>
-		Crevatia was founded some time around <script>sy(2012,4)</script> as the Dominion of Crevatia. In 
-		<script>sy(2016,4)</script> the dominion annexed the villages in the northwest and formed the new 
-		Northwest Territory. In <script>sy(2016,5,7)</script> the dominion was overthrown by the military 
-		and the Democratic People's Republic of Crevatia was established, transforming from a feudal 
-		imperial state to its current form:
-		<blockquote>
-			<i>"The Crevatians have spoken! On behalf of the disenfranchised citizens of Crevatia, the 
-			military has overthrown the Dominion and imposed martial law.  The new republic of 
-			Crevatia is in a state of violent flux, and the governors themselves (Frankerstein, 
-			Nomers6 and Gickitty) threatened to secede. But with deft political maneuvering, bribes 
-			and the threat of economic isolation, one governor after another was swayed to remain in 
-			the federation. Old flags are being torn down, new banners and soldiers are everywhere, 
-			and the old vestiges of the dominion are being swept away as we speak. The old imperial 
-			temple is now being refurbished as a hall of heroes for the inspiration of the populace.
-			<br><br>
-			Travel into the new republic may be disrupted as terrorist bombings have been a way of 
-			life in several neighborhoods. But the Crevatians will not cower in fear! Already, they 
-			have constructed several new symbols of hope for the future. The Lyceum beacon now rotates 
-			through several colors, the mall clock now plays 'ode to joy' at noon, and home tree is 
-			growing. Big. Wearing Elytra and jumping off from the highest points, it is possible to 
-			reach Anderson's Crossing, Delve, and almost Lunar Hill.
-			<br><br>
-			The future of Crevatia is now directly in the hands of the people and regional governors, 
-			and with the help of the Military, this glorious future will be guaranteed. Long life to 
-			the Republic!"</i>
-		</blockquote>
-	</p>
-	<p>
-		Crevatia is home to five players: yrsyrs, gickitty, frankerstein, Nomers6, and UltimateKillerKG.
-	</p>
-	<br>
-	
-	<div class="contentheader round">Regions of Crevatia</div>
-	<script>
-		infowindow("Senatus populusque crevatiae","Crevasse","","Crevatian","Federal Militarist Totalitarian Democracy","","Denarius (&#x110;)","Crevatian Catholic Rite","Eagle","Eweton Cathedral (31m)","Hometree (Crevasse, 44m)");
-		regions("Crevatia","Region",["Capital","Provinces","Frontier","Northwest Territory"],["Crevasse","Anderson's Crossing","Gickerville","Pontia"],[["Baetica","Bentley Bay","CPC Power Plant","Eweton","Palace Pyramid","Volcano","William's Harbour"],["Fort Resolution","Dirt Village","Noricum","Stellarton","The Trident"],["Fort Sungard"],["Campania","Narbo"]],1);
-	</script>
-
-	<div style="clear:both"></div>
-
-<?php include "../data/footer.htm" ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Crevatia</title>
+		<link rel="stylesheet" href="../../data/mc.css">
+		<link rel="icon" href="../../images/pickaxe.png" type="image/x-icon">
+		<script src="../../data/time.js"></script>
+		<script src="../../data/world.js"></script>
+	</head>
+	<body>
+		<header>
+			<img id="stateflag" height="150" class="gallery" style="display:block; margin: 20px auto;">
+		</header>
+		<main>
+			<div class="content round shadow">
+				<div id="statetitle" class="contentheader round"></div>
+				<div class="gallery">
+					<div style="width: 600px;">
+						<p>
+							Crevatia was founded some time around <script>sy(2012,4)</script> as the Dominion of Crevatia. In <script>sy(2016,4)</script> the dominion 
+							annexed the villages in the northwest and formed the new Northwest Territory. In <script>sy(2016,5,7)</script> the dominion was overthrown by 
+							the military and the Democratic People's Republic of Crevatia was established, transforming from a feudal imperial state to its current form:
+						</p>
+						<blockquote>
+							"The Crevatians have spoken! On behalf of the disenfranchised citizens of Crevatia, the military has overthrown the Dominion and imposed martial 
+							law. The new republic of Crevatia is in a state of violent flux, and the governors themselves (Frankerstein, Nomers6, and Gickitty) threatened 
+							to secede. But with deft political maneuvering, bribes, and the threat of economic isolation, one governor after another was swayed to remain in 
+							the federation. Old flags are being torn down, new banners and soldiers are everywhere, and the old vestiges of the dominion are being swept 
+							away as we speak. The old imperial temple is now being refurbished as a hall of heroes for the inspiration of the populace.
+						</blockquote>
+						<blockquote>
+							Travel into the new republic may be disrupted as terrorist bombings have been a way of life in several neighborhoods. But the Crevatians will 
+							not cower in fear! Already, they have constructed several new symbols of hope for the future. The Lyceum beacon now rotates through several 
+							colours, the mall clock now plays 'Ode to Joy' at noon, and Home Tree is growing - Big. Wearing Elytra and jumping off from the highest points, 
+							it is possible to reach Anderson's Crossing, Delve, and almost Lunar Hill.
+						</blockquote>
+						<blockquote>
+							The future of Crevatia is now directly in the hands of the people and regional governors, and with the help of the Military, this glorious future 
+							will be guaranteed. Long life to the Republic!"
+						</blockquote>
+						<p>
+							Crevatia is home to five players: yrsyrs, gickitty, frankerstein, Nomers6, and UltimateKillerKG.
+						</p>
+					</div>
+					<div id="infowindow" class="right" style="max-width: 300px;"></div>
+				</div>
+			</div>
+		</main>
+		<script>
+			worldData("Crevatia");
+			regions("Crevatia","Region",["Capital","Provinces","Frontier","Northwest Territory"],["Crevasse","Anderson's Crossing","Gickerville","Pontia"],[["Baetica","Bentley Bay","CPC Power Plant","Eweton","Palace Pyramid","Volcano","William's Harbour"],["Fort Resolution","Dirt Village","Noricum","Stellarton","The Trident"],["Fort Sungard"],["Campania","Narbo"]],1);
+		</script>
+	</body>
+</html>
