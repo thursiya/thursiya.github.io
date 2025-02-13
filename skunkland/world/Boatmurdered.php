e781d2c2f5dcb7334745a72649d9e4775c84e1c4
thursiya <thursiya.exe@gmail.com>
2025-02-12 19:54:07 -0800

    Update Boatmurdered.php

--- skunkland/world/Boatmurdered.php
+++ skunkland/world/Boatmurdered.php
@@ -1,23 +1,40 @@
-<?php $nation="Boatmurdered"; $ntype="Miasma"; include '../data/header-world.htm' ?>
-
-	<script>
-		infowindow("","None","","Hapless Souls","Lawless Frontier State","","Barter Economy","Widespread petitions to various higher powers","","Lut Gholein Workshop (8m)","Boatmurdered Skull Entrance (18m)");
-	</script>
-	
-	<p>
-		Boatmurdered was founded in <script>sy(2014,4,17,19)</script>.
-	</p>
-	<blockquote>
-		<i>"The townsfolk of Lut Gholein were a simple, happy folk and often wouldn't even notice or 
-		perhaps pretended to ignore the sounds of madness that wafted in on the early morning winds. 
-		Every day started with the shadow of the ominous spire, the sun slowly peeling away its moist 
-		cloying darkness. Inquiries from strangers about the darkened eastern road would often go 
-		unanswered.  Legends and myths of the creatures that dwell down that road keep the children 
-		safe in their beds.  And young men boasting of bravery march down the road as far as they dare. 
-		Some never return."</i>
-	</blockquote>
-	<br>
-	
-	<div style="clear:both"></div>
-
-<?php include "../data/footer.htm" ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Boatmurdered</title>
+		<link rel="stylesheet" href="../../data/mc.css">
+		<link rel="icon" href="../../images/pickaxe.png" type="image/x-icon">
+		<script src="../../data/time.js"></script>
+		<script src="../../data/world.js"></script>
+	</head>
+	<body>
+		<header>
+			<img id="stateflag" height="150" class="gallery" style="display:block; margin: 20px auto;">
+		</header>
+		<main>
+			<div class="content round shadow">
+				<div id="statetitle" class="contentheader round"></div>
+				<div class="gallery">
+					<div style="width: 600px;">
+						<p>
+							Boatmurdered was founded in <script>sy(2014,4,17,19)</script>.
+						</p>
+						<blockquote>
+							"The townsfolk of Lut Gholein were a simple, happy folk and often wouldn't even notice or perhaps pretended to ignore the sounds of madness 
+							that wafted in on the early morning winds. Every day started with the shadow of the ominous spire, the sun slowly peeling away its moist 
+							cloying darkness. Inquiries from strangers about the darkened eastern road would often go unanswered. Legends and myths of the creatures 
+							that dwell down that road keep the children safe in their beds. And young men boasting of bravery march down the road as far as they dare. 
+							Some never return."
+						</blockquote>
+					</div>
+					<div id="infowindow" class="right" style="max-width: 300px;"></div>
+				</div>
+			</div>
+		</main>
+		<script>
+			worldData("Boatmurdered");
+		</script>
+	</body>
+</html>
