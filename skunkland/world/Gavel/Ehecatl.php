985cc986b788a4939ea83b63d57adbef884402a8
thursiya <thursiya.exe@gmail.com>
2025-02-15 00:21:46 -0800

    Update Ehecatl.php

--- skunkland/world/Gavel/Ehecatl.php
+++ skunkland/world/Gavel/Ehecatl.php
@@ -1,31 +1,39 @@
-<?php $region="Ehecatl"; $rtype="Special Administrative Region"; include '../../data/header-region.htm'; ?>
-
-	<script>
-		settlements('None');
-	</script>
-	
-	<a href="../../maps/Ehecatl.png" target="_blank"><img src="../../maps/Ehecatl.png" alt="Map of Ehecatl SAR" height="300"  style="display:block; margin:auto auto"></a>
-	<br>
-	<p>
-		Before joining the Republic of Gavel, Ehecatl enjoyed centuries as a prosperous city state  
-		dominating the Boreal Ocean.  But with the rise of Gavel's new showpiece city, Metropolis, to 
-		the west and the increasing reliance of the nearby coastal communities on the republic's 
-		resources, Ehecatl was eclipsed and its economy shrank.  Many states may have been able to 
-		weather such an economic downturn, especially since the belligerent Animan Empire had 
-		transformed into the rather peaceful Gavel Republic, however Ehecatl's prosperity had allowed
-		a society devoted to the pursuit of knowledge to flourish and the academics of its now 
-		all-powerful University of Ehecatl were loathe to give up that lifestyle. So a deal was struck 
-		with the city state's powerful neighbour where Ehecatl would give up some of its autonomy and 
-		expertise for Gavel's financial and military support. Soon academics from across the republic 
-		flocked to Ehecatl and its island-spanning university in the hopes of escaping a little of the 
-		attention of Gavel's secret police.  The special administrative region of Ehecatl is in the 
-		north of Gavel, covering the Isle of Ehecatl, and is bordered by the Boreal Ocean to the north.
-	</p>
-	<br>
-	<a href="../../newsletters/images/University of Ehecatl.jpg" target="_blank"><img src="../../newsletters/images/University of Ehecatl.jpg" alt="University of Ehecatl" height="200" style="display:block; margin:auto auto"></a>
-	<br>
-	<p>
-		- <a href="../../gallery/Gavel.php#Ehecatl">Other Images</a> -
-	</p>
-
-<?php include "../../data/footer.htm" ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Ehecatl Special Administrative Region</title>
+		<link rel="stylesheet" href="../../data/mc.css">
+		<link rel="icon" href="../../images/pickaxe.png" type="image/x-icon">
+		<script src="../../data/time.js"></script>
+		<script src="../../data/world.js"></script>
+	</head>
+	<body>
+		<header>
+			<img id="regionflag" height="150" class="gallery" style="display:block; margin: 20px auto;">
+		</header>
+		<main>
+			<div class="content round shadow">
+				<div id="regiontitle" class="contentheader round"></div>
+				<div class="gallery">
+					<div id="regionmap" style="flex: 1;"></div>
+					<div id="infowindow" class="right" style="max-width: 400px;"></div>
+				</div>
+				<p>
+					Before joining the Republic of Gavel, Ehecatl enjoyed centuries as a prosperous city-state dominating the Boreal Ocean. But with the rise of Gavel's new showpiece 
+					city, Metropolis, to the west, and the increasing reliance of the nearby coastal communities on the republic's resources, Ehecatl was eclipsed and its economy shrank. 
+					Many states may have been able to weather such an economic downturn, especially since the belligerent Animan Empire had transformed into the rather peaceful Gavel 
+					Republic, however Ehecatl's prosperity had allowed a society devoted to the pursuit of knowledge to flourish, and the academics of its now all-powerful University of 
+					Ehecatl were loathe to give up that lifestyle. So a deal was struck with the city-state's powerful neighbour where Ehecatl would give up some of its autonomy and 
+					expertise for Gavel's financial and military support. Soon academics from across the republic flocked to Ehecatl and its island-spanning university in the hopes of 
+					escaping a little of the attention of Gavel's secret police. The special administrative region of Ehecatl is in the north of Gavel, covering the Isle of Ehecatl, and 
+					is bordered by the Boreal Ocean to the north.
+				</p>
+			</div>
+		</main>
+		<script>
+			settlements("Gavel", "Ehecatl", "Special Administrative Region");
+		</script>
+	</body>
+</html>
