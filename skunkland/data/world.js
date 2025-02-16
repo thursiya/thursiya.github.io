e6ebc2df8b91fc2a4c9398d5d07d1a916e505775
thursiya <thursiya.exe@gmail.com>
2025-02-15 16:11:12 -0800

    Update world.js

--- skunkland/data/world.js
+++ skunkland/data/world.js
@@ -35,7 +35,7 @@ function drawInfoWindow(state) {
 		return `${stOut}</table></td></tr>`;
 	}
 
-	const bigcity = city.toSorted((a, b) => b.pop - a.pop).toSorted((a, b) => a.state.localeCompare(b.state)).filter(v => v.state == state.name)[0] || { name: "None", output: { pop: "Uninhabited" } };
+	const bigcity = city.slice(0).sort((a, b) => b.pop - a.pop).sort((a, b) => a.state.localeCompare(b.state)).filter(v => v.state == state.name)[0] || { name: "None", output: { pop: "Uninhabited" } };
 	let out = `<table style="width: 300px;"><tr><th>${state.type} of ${state.name}</th></tr>
  		<tr><td><img src="../images/flags/${state.name} (Wool).png" width="150"><br><i>${state.motto || "---"}</i></td></tr>
    		<tr><td style="padding: 0;"><img src="../../maps/regional/${state.name}.jpg" width="298"></td></tr>
