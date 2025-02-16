cb3820d0fac166f8ccec480823f3df2a9428c22c
thursiya <thursiya.exe@gmail.com>
2025-02-15 22:08:34 -0800

    Update Ghibli.php

--- skunkland/world/Gavel/Ghibli.php
+++ skunkland/world/Gavel/Ghibli.php
@@ -1,24 +1,36 @@
-<?php $region="Ghibli"; $rtype="Province"; include '../../data/header-region.htm'; ?>
-
-	<script>
-		settlements(['Lunar Hill','Ghibli Port'],[new Date(2012,7,10),new Date(2012,6,19)]);
-	</script>
-	
-	<a href="../../maps/Ghibli.png" target="_blank"><img src="../../maps/Ghibli.png" alt="Map of Ghibli Province" height="300"  style="display:block; margin:auto auto"></a>
-	<br>
-	<p>
-		The province of Ghibli was formed as part of the transformation of the Animan Empire into a 
-		modern republic.  The region comprising modern Ghibli was originally a hinterland between the 
-		kingdoms of Pristina and Masovia, home to Romani in the north and to this day the province 
-		remains one of the most culturally diverse in Gavel.  Politics in Ghibli are dominated by the 
-		Tsai Group who control most of the province's major industries.  The province of Ghibli is in 
-		the south of Gavel and borders Koku to the south and South Shore to the east.
-	</p>
-	<br>
-	<a href="../../gallery/images/Ghibli Port.jpg" target="_blank"><img src="../../gallery/images/Ghibli Port.jpg" alt="Ghibli Port" height="200"  style="display:block; margin:auto auto"></a>
-	<br>
-	<p>
-		- <a href="../../gallery/Gavel.php#Ghibli">Other Images</a> -
-	</p>
-
-<?php include "../../data/footer.htm" ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Ghibli Province</title>
+		<link rel="stylesheet" href="../../data/mc.css">
+		<link rel="icon" href="../../images/pickaxe.png" type="image/x-icon">
+		<script src="../../data/time.js"></script>
+		<script src="../../data/world.js"></script>
+	</head>
+	<body>
+		<header><img id="regionflag" class="header"></header>
+		<main>
+			<div class="content round shadow">
+				<div id="regiontitle" class="contentheader round"></div>
+				<div class="gallery">
+					<div id="regionmap" style="flex: 1;"></div>
+					<div id="infowindow" class="right" style="max-width: 400px;"></div>
+				</div>
+				<p> 
+					The province of Ghibli was formed as part of the transformation of the Animan Empire into a modern republic. The region comprising modern Ghibli was originally a 
+					hinterland between the kingdoms of Pristina and Masovia, home to Romani in the north, and to this day, the province remains one of the most culturally diverse in 
+					Gavel. Politics in Ghibli are dominated by the Tsai Group, who control most of the province's major industries. The province of Ghibli is in the south of Gavel 
+					and borders Koku to the south and South Shore to the east.
+				</p>
+				<script>pic("Ghibli Port")</script>
+			</div>
+		</main>
+		<script>
+			settlements("Gavel", "Ghibli", "Province", [
+				{ name: "Lunar Hill", founded: syear(2012,7,10) },
+				{ name: "Ghibli Port", founded: syear(2012,6,19) }]);
+		</script>
+	</body>
+</html>
