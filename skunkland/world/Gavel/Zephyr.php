791be177461df474f9e3ed43c1ccd99ae95bb1ec
thursiya <thursiya.exe@gmail.com>
2025-02-15 22:34:21 -0800

    Update Zephyr.php

--- skunkland/world/Gavel/Zephyr.php
+++ skunkland/world/Gavel/Zephyr.php
@@ -1,25 +1,35 @@
-<?php $region="Zephyr"; $rtype="Province"; include '../../data/header-region.htm'; ?>
-
-	<script>
-		settlements(['Zefram'],[new Date(2015,11,17)]);
-	</script>
-
-	<a href="../../maps/Zephyr.png" target="_blank"><img src="../../maps/Zephyr.png" alt="Map of Zephyr Province" height="300"  style="display:block; margin:auto auto"></a>
-	<br>
-	<p>
-		The province of Zephyr was formed as part of the transformation of the Animan Empire into a 
-		modern republic.  The region comprising modern Zephyr was the original homeland of the 
-		forest dwellers that joined the central tribes to form the Animan Empire.  Before the Great 
-		Warming Zephyr was a forbidding land and its people hardened warriors.  Today it houses the 
-		Zephyr Federal Mining Penal Colony, the paramount prison facility in Gavel.  The central 
-		forest area is now home to the province's largest settlement, Zefram.  The province of 
-		Zephyr is located in the west of Gavel, bordering Crevatia.
-	</p>
-	<br>
-	<a href="../../gallery/images/Zephyr Prison.jpg" target="_blank"><img src="../../gallery/images/Zephyr Prison.jpg" alt="Zephyr Federal Prison" height="200"  style="display:block; margin:auto auto"></a>
-	<br>
-	<p>
-		- <a href="../../gallery/Gavel.php#Zephyr">Other Images</a> -
-	</p>
-
-<?php include "../../data/footer.htm" ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Zephyr Province</title>
+		<link rel="stylesheet" href="../../data/mc.css">
+		<link rel="icon" href="../../images/pickaxe.png" type="image/x-icon">
+		<script src="../../data/time.js"></script>
+		<script src="../../data/world.js"></script>
+	</head>
+	<body>
+		<header><img id="regionflag" class="header"></header>
+		<main>
+			<div class="content round shadow">
+				<div id="regiontitle" class="contentheader round"></div>
+				<div class="gallery">
+					<div id="regionmap" style="flex: 1;"></div>
+					<div id="infowindow" class="right" style="max-width: 400px;"></div>
+				</div>
+				<p> 
+					The province of Zephyr was formed as part of the transformation of the Animan Empire into a modern republic. The region comprising modern Zephyr was the original 
+					homeland of the forest dwellers that joined the central tribes to form the Animan Empire. Before the Great Warming, Zephyr was a forbidding land and its people 
+					hardened warriors. Today, it houses the Zephyr Federal Mining Penal Colony, the paramount prison facility in Gavel. The central forest area is now home to the 
+					province's largest settlement, Zefram. The province of Zephyr is located in the west of Gavel, bordering Crevatia.
+				</p>
+				<script>pic("Zefram at Night")</script>
+			</div>
+		</main>
+		<script>
+			settlements("Gavel", "Zephyr", "Province", [
+				{ name: "Zefram", founded: syear(2015,11,17) }]);
+		</script>
+	</body>
+</html>
