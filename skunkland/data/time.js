05020ef09a13d19a81483c268486ada4fc4b431b
thursiya <thursiya.exe@gmail.com>
2025-02-15 19:28:11 -0800

    Update time.js

--- skunkland/data/time.js
+++ skunkland/data/time.js
@@ -4,7 +4,7 @@ function syear(day, val2, val3, val4) {
 		if (!val3) val2 = 'noday';
 	}
 	const lsy = (day.getFullYear() - 2012 + ((day - new Date(day.getFullYear(),0,1)) / 86400000 + 61) / 365) * 72 + 1;
-	return `<span title='${day.toLocaleString("en",{month:"long"})} ${val2 != "noday" ? `${day.getDate()}, ` : ""}${day.getFullYear()}'>${val2 == "full" || val4 ? `${new Date(~~lsy, ~~(lsy % 1 * 12)).toLocaleString("en",{month:"long"})} of ` : ""}SY ${~~lsy}</span>`;
+	return `<span title="${day.toLocaleString("en", { month:"long" })} ${val2 != "noday" ? `${day.getDate()}, ` : ""}${day.getFullYear()}">${val2 == "full" || val4 ? `${new Date(~~lsy, ~~(lsy % 1 * 12)).toLocaleString("en",{month:"long"})} of ` : ""}SY ${~~lsy}</span>`;
 }
 
 function sy(a, b, c, d) { document.write(syear(a, b, c, d)) }
