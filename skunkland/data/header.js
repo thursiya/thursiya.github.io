d54e3ff9bb698c51896131f81d0a6afd8325681d
thursiya <thursiya.exe@gmail.com>
2025-02-17 13:25:38 -0800

    Update header.js

--- skunkland/data/header.js
+++ skunkland/data/header.js
@@ -9,8 +9,8 @@ function insertHeader() {
 			<span style="vertical-align: middle">${syear(new Date(),"full")}</span>
 		</div>
 
-		<div class="header round shadow" style="background: url(${homepage}images/vistas/bgvista${~~((Math.random() * 13) + 1)}.jpg)">
-			<img src="${homepage}images/server_name.gif" style="display: block; margin: auto; padding-top: 50px">
+		<div class="header round shadow" style="background: url(${homepage}images/vistas/bgvista${~~((Math.random() * 13) + 1)}.jpg) top">
+			<img src="${homepage}images/server_name.gif" style="display: block; margin: auto; max-width: 100%; padding-top: 50px; width: 100vw;">
 		</div>
 
 		<div class="navbar round shadow">
