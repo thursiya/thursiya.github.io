f95e165abb89c505c804e68b09d334709df3e1c0
thursiya <thursiya.exe@gmail.com>
2025-03-16 22:14:19 -0700

    Create nicoma.js

---
+++ textgames/nicoma.js
@@ -0,0 +1,51 @@
+const nicoma = {};
+
+function initNicoma () {
+	currentApp = "Nicoma";
+	gameLog.length = 0;
+	document.getElementById("textInput").type = "text";
+	
+	updateLog(`<b>NICOMA</b><br><i>Based on the Creative Computing game</i><br><br>Boomerang Puzzle from Arithmetica of Nicomachus -- A.D. 90!`);
+	
+  sumbitNicoma("Intro");
+}
+
+function submitNicoma(state) {
+  const data = textInput.value.toUpperCase();
+	if (!data) return;
+	const num = isNaN(data) ? -1 : Math.floor(data);
+	switch (state || gameState) {
+    case "Intro":
+      updateLog(`<hr>Please think of a number between 1 and 100.`);
+      updateLog(`Your number divided by <b>3</b> has a remainder of:`);
+      gameState = 3;
+      break;
+		case 3:
+    case 5:
+    case 7:
+      if (num >= 0 && num < gameState) {
+        appendLog(num);
+        nicoma[gameState] = num;
+        gameState += 2;
+        if (gameState < 8) {
+          updateLog(`Your number divided by <b>${gameState}</b> has a remainder of:`);
+        } else {
+          updateLog(`Let me think a moment...`);
+          nicoma.num = nicoma[3] * 70 + nicoma[5] * 21 + nicoma[7] * 15;
+          if (nicoma.num > 100) nicoma.num -= 105;
+          updateLog(`Your number was <b>${nicoma.num}</b>, right?`);
+          gameState = "Approve Guess";
+        }
+      }
+			break;
+		case "Approve Guess":
+      appendLog(data[0] == "Y" ? "Yes" : "No");
+      updateLog(data[0] == "Y" ? `How about that!!` : `I feel your arithmetic is in error.`);
+      updateLog(`Let's try another!`);
+      submitNicoma("Intro");
+			break;
+		default:
+			updateLog(`<i>Reload the game to restart</i>`);
+	}
+	setInput();
+}
