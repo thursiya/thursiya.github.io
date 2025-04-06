e08a6c25e3ced40dd2aa64ed5397d04b23af0476
thursiya <thursiya.exe@gmail.com>
2025-04-06 16:32:15 -0700

    Update info.js

--- spacegame/info.js
+++ spacegame/info.js
@@ -2,38 +2,43 @@ let newsItem = [];
 let newsFramework = [];
 let lastNewsEvent = 0;
 
-
-function loadNewsFramework () {
-	newsFramework = Array.apply(0, {length: world.length}).map(Number.call, Number);
-	newsFramework.push(...newsFramework, ...newCorps.map(v => v.name), ...oldCorps.map(v => v.name));
+// Generate shuffled array with two copies of each world number and one copy of each corp name (for news topics)
+// Consider changing distribution to reflect world pop?
+function loadNewsFramework() {
+	newsFramework = [...Array(world.length)].map((_, i) => i);
+	newsFramework = shuffle([...newsFramework, ...newsFramework, ...newCorps.map(v => v.name), ...oldCorps.map(v => v.name)]);
 }
 
-function newsEvent (nTime = time.full) {
+function newsEvent(nTime = time.full) {
 	if (newsFramework.length < 1) loadNewsFramework();
-	const subject = newsFramework.splice(rnd(newsFramework.length) - 1, 1)[0];
+	const subject = newsFramework.pop();
+	let shortSubject = "";
 	let headline = "";
 	let text = "";
-	
+
+	// Planet News
 	if (Number.isInteger(subject)) {
-		var shortSubject = world[subject].name;
-		headline = rnd(["Famine on",
+		shortSubject = world[subject].name;
+		headline = `${rnd(["Famine on",
 			"Influx of Refugees to",
 			"Military Lockdown on",
 			"Tourists Flocking to",
 			"Ancient Colony Discovered on",
-			"New Job Opportunities on"]) + " " + shortSubject;
+			"New Job Opportunities on"])} ${shortSubject}`;
+	// Corporate News
 	} else {
-		var shortSubject = subject.split(" ", 1);
-		let possibleGoods = [];
-		goods.forEach(v => {if (v.type == shortSubject) possibleGoods.push(v.name)});
-		const good = rnd(possibleGoods);
-		headline = shortSubject + " " + rnd(
+		shortSubject = subject.split(" ", 1);
+		//const possibleGoods = [];
+		//goods.forEach(v => {if (v.type == shortSubject) possibleGoods.push(v.name)});
+		//const good = rnd(possibleGoods);
+		const good = rnd(goods.reduce((t, v) => v.type == shortSubject ? [...t, v.name] : t, []));
+		headline = `${shortSubject} ${rnd(
 			[`Stocks Up`, 
 			`Caught up in Investigation`, 
 			`Faces Scrutiny from Shareholders`, 
-			`Turns ${rnd(['Massive', 'Healthy', 'Sizeable', 'Large'])} Profit ${rnd(['after Cutting Costs', 'in ' + (['1st', '2nd', '3rd'][~~(time.day / 92) - 1] || '4th') + ' Quarter', 'with New Leadership'])}`, 
-			`to Build New ${good ? good + ' Plant' : rnd(['Facility', 'Offices', 'Residences'])}`, 
-			`R&D Has Big Announcement`]);
+			`Turns ${rnd(["Massive", "Healthy", "Sizeable", "Large"])} Profit ${rnd(["after Cutting Costs", `in ${["1st", "2nd", "3rd"][~~(time.day / 92) - 1] || "4th"} Quarter`, "with New Leadership"])}`, 
+			`to Build New ${good ? `${good} Plant` : rnd(["Facility", "Offices", "Residences"])}`,
+			`R&D Has Big Announcement`])}`;
 		text = "";
 	}
 	
