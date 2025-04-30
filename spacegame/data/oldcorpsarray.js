9d9cf7fcdfe3f579f1fd3bd21333486225bc8e21
thursiya <thursiya.exe@gmail.com>
2025-04-30 13:05:23 -0700

    Update oldcorpsarray.js

--- spacegame/data/oldcorpsarray.js
+++ spacegame/data/oldcorpsarray.js
@@ -1,4 +1,14 @@
 const oldCorps = [
+  { name: "5-Star", type: "Conglomerate", founded: 2086, hq: "Greater Busan Aerotropolis, Earth", motto: "Reliable Tomorrow. Everywhere.", desc: `
+    <p>Born from the merger of several legacy Korean chaebols during the Great Consolidation of the 2080s, 5-Star Conglomerate quickly rose to become the premier supplier of dependable, mid-tier goods for the 
+    expanding interstellar middle class. Built on the industrial DNA of old-world giants, 5-Star's early slogan—<i>"Powering Progress, Planet to Planet"</i>—still defines its mission.</p>
+    <p>Specializing in consumer electronics, smart vehicles, and snack foods, 5-Star is best known for its GaonDrive line of autonomous interplanetary vehicles and the ubiquitous NeoLife smart ecosystem, which 
+    connects everything from personal devices to home bots to shipboard systems. Though never the flashiest, 5-Star's offerings are prized for their solid engineering, long warranties, and relentless software 
+    updates (whether you want them or not).</p>
+    <p>The corporation's vertical integration is legendary: from asteroid mining to AI chip design, from consumer drones to deep-space construction equipment, 5-Star does it all under one sprawling umbrella. Its 
+    flagship city-sized arcology in orbit over Busan serves as both HQ and living showroom, featuring rotating exhibits of concept vehicles and domestic tech demonstrations for visiting dignitaries.</p>
+    <p>Operating in 63 planetary markets and 11 deep-space hubs, 5-Star remains a symbol of East Asian industrial ambition made galactic. It's also a major cultural force, producing branded entertainment, virtual 
+    idols, and sponsoring the Interstellar Drift League—a sport made possible by their popular Gaon-7X gravcar.</p>` },
   { name: "Mitsutomo", type: "Zaibatsu", founded: 2094, hq: "Shinhama City, Amaji", motto: "Shape the Future. Serve the Stars.", desc: `
     <p>Mitsutomo began as a small robotics R&D firm in 2094, founded by visionary engineer Aika Mitsutomo and a coalition of post-national investors. Within a few decades, it grew into one of the most powerful
     megacorporations in known space, renowned for its cutting-edge robotics, adaptive AI platforms, and modular terraforming infrastructure.</p>
